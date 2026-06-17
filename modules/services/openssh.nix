74727aa9b9ab898a1e4621cdf1c4f95a417b6881
arca0928 <arca0928@gmail.com>
2026-06-18 00:12:01 +0900

    feat: add openssh server

---
+++ modules/services/openssh.nix
@@ -0,0 +1,27 @@
+{ delib, host, ... }:
+delib.module {
+  name = "openssh";
+
+  options = delib.singleEnableOption (host.isDesktop or host.isServer);
+
+  nixos.ifEnabled =
+    { myconfig, ... }:
+    {
+      services.openssh = {
+        enable = true;
+
+        hostKeys = [
+          {
+            path = "/etc/ssh/ssh_host_ed25519_key";
+            type = "ed25519";
+          }
+        ];
+
+        settings = {
+          PasswordAuthentication = false;
+          AllowUsers = [ "${myconfig.constants.username}" ];
+          PermitRootLogin = "no";
+        };
+      };
+    };
+}
