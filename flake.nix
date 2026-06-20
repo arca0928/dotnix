8ecebe0967d3b4e43de4425bd95587893f5e598c
arca0928 <arca0928@gmail.com>
2026-06-20 17:05:29 +0900

    feat: add xremap

--- flake.nix
+++ flake.nix
@@ -87,6 +87,7 @@
                       "bluetooth"
                       "secureboot"
                       "fingerprint"
+                      "xremap"
                     ];
                     defaultByHostType = {
                       desktop = [
---
+++ modules/services/xremap.nix
@@ -0,0 +1,72 @@
+{
+  delib,
+  host,
+  inputs,
+  ...
+}:
+delib.module {
+  name = "services.xremap";
+
+  options = delib.singleEnableOption host.xremapFeatured;
+  nixos.always.imports = [ inputs.xremap.nixosModules.default ];
+  nixos.ifEnabled = {
+    services.xremap = {
+      enable = true;
+
+      config = {
+        modmap = [
+          {
+            remap = {
+              "CapsLock" = "Ctrl_L";
+            };
+          }
+        ];
+        keymap = [
+          {
+            name = "Onishi";
+            device = {
+              only = "AT Translated Set 2 keyboard";
+            };
+            remap = {
+              "MINUS" = "SLASH";
+              "Q" = "Q";
+              "W" = "L";
+              "E" = "U";
+              "R" = "COMMA";
+              "T" = "DOT";
+              "Y" = "F";
+              "U" = "W";
+              "I" = "R";
+              "O" = "Y";
+              "P" = "P";
+              "A" = "E";
+              "S" = "I";
+              "D" = "A";
+              "F" = "O";
+              "G" = "MINUS";
+              "H" = "K";
+              "J" = "T";
+              "K" = "N";
+              "L" = "S";
+              "SEMICOLON" = "H";
+              "Z" = "Z";
+              "X" = "X";
+              "C" = "C";
+              "V" = "V";
+              "B" = "SEMICOLON";
+              "N" = "G";
+              "M" = "D";
+              "COMMA" = "M";
+              "DOT" = "J";
+              "SLASH" = "B";
+            };
+            mode = [
+              "ONISHI"
+            ];
+          }
+        ];
+        default_mode = "ONISHI";
+      };
+    };
+  };
+}
