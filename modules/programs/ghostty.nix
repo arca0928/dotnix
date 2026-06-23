9793bebeb3e3d0bcd5e53b957dad02ace685323d
arca0928 <git@arca0928.dev>
2026-06-23 23:48:04 +0900

    feat: ghosttyの設定追加

--- modules/programs/ghostty.nix
+++ modules/programs/ghostty.nix
@@ -7,6 +7,29 @@ delib.module {
   home.ifEnabled = {
     programs.ghostty = {
       enable = true;
+      enableZshIntegration = true;
+
+      settings = {
+        font-family = "Moralerspace Neon";
+        font-feature = [
+          "+liga"
+          "+calt"
+          "+ss01"
+          "+ss02"
+          "+ss03"
+          "+ss04"
+          "+ss05"
+          "+ss06"
+          "+ss07"
+          "+ss08"
+          "+ss09"
+          "+ss10"
+        ];
+        theme = "0x96f";
+        cursor-style = "block";
+        cursor-style-blink = false;
+        shell-integration-features = "no-cursor";
+      };
     };
   };
 }
