37dc0066e88c3168f78a4437789f96dfbb7a4c67
arca0928 <git@arca0928.dev>
2026-07-04 15:55:48 +0900

    feat: change discord client

---
+++ modules/programs/discord.nix
@@ -0,0 +1,19 @@
+{
+  delib,
+  host,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "programs.discord";
+  options = delib.singleEnableOption host.guiFeatured;
+
+  home.ifEnabled = {
+    home.packages = [
+      (pkgs.discord.override {
+        withEquicord = true;
+        withOpenASAR = true;
+      })
+    ];
+  };
+}
