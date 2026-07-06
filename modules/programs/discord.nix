a92b6cdfd06377a95bf4977fa57f5012c8b54095
arca0928 <git@arca0928.dev>
2026-07-07 00:15:48 +0900

    fix: 日本語IME用のflagsを追加

--- modules/programs/discord.nix
+++ modules/programs/discord.nix
@@ -13,6 +13,7 @@ delib.module {
       (pkgs.discord.override {
         withEquicord = true;
         withOpenASAR = true;
+        commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime";
       })
     ];
   };
