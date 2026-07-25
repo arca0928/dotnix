5ab3ae162eb2fbf39f81dc9bee7cd7c53f7b4f16
arca0928 <git@arca0928.dev>
2026-07-25 16:06:55 +0900

    fix(emacs): respect server option

--- modules/programs/emacs.nix
+++ modules/programs/emacs.nix
@@ -13,18 +13,20 @@ delib.module {
   };
 
   home.always.imports = [ inputs.twist.homeModules.emacs-twist ];
-  home.ifEnabled = {
-    home.packages = [
-      inputs.lsp-proxy.packages.${pkgs.stdenv.hostPlatform.system}.default
-    ];
-    programs.emacs-twist = {
-      enable = true;
-      config = inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.my-emacs;
-      createInitFile = true;
-      earlyInitFile = pkgs.tangleOrgBabelFile "early-init.el" ../../partitions/emacs/early-init.org { };
-      createManifestFile = true;
-      emacsclient.enable = true;
-      serviceIntegration.enable = true;
+  home.ifEnabled =
+    { cfg, ... }:
+    {
+      home.packages = [
+        inputs.lsp-proxy.packages.${pkgs.stdenv.hostPlatform.system}.default
+      ];
+      programs.emacs-twist = {
+        enable = true;
+        config = inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.my-emacs;
+        createInitFile = true;
+        earlyInitFile = pkgs.tangleOrgBabelFile "early-init.el" ../../partitions/emacs/early-init.org { };
+        createManifestFile = true;
+        emacsclient.enable = cfg.enableServer;
+        serviceIntegration.enable = cfg.enableServer;
+      };
     };
-  };
 }
